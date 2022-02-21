af59ce7f000cc5ad4b7641319c98e4a8665a766b
Wiltei <vilho.teivainen@gmail.com>
2022-02-21 14:28:07 +0200

    Sololearn.com React+Redux  project, for easy access.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+/* ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
+); */
+
+let counter = 0;
+
+function show() {
+  counter++;
+  const el = <p>{counter}</p>;
+  ReactDOM.render(el, document.getElementById("root"));
+}
+
+setInterval(show, 1000);
+ReactDOM.render(
+  <h1>Hello, React!</h1>,
+
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
